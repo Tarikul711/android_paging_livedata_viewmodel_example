55da9ce8ec8946b177b44b09716f8554d31dcd2c
Tarikul711 <Tarikul711@gmail.com>
2020-07-26 13:15:43 +0600

    Add repo

--- app/src/main/java/com/tos/retrofitokhttpcaching/viewmodel/QuestionViewModel.java
+++ app/src/main/java/com/tos/retrofitokhttpcaching/viewmodel/QuestionViewModel.java
@@ -1,16 +1,21 @@
 package com.tos.retrofitokhttpcaching.viewmodel;
 
+import com.tos.retrofitokhttpcaching.model.question.Item;
 import com.tos.retrofitokhttpcaching.model.question.QuestionData;
 import com.tos.retrofitokhttpcaching.repository.QuestionRepository;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.paging.PageKeyedDataSource;
+import androidx.paging.PagedList;
 
 /**
  * Created by tarikul on 24/7/20
  */
 public class QuestionViewModel extends ViewModel {
     MutableLiveData<QuestionData> data;
+    MutableLiveData<PagedList<Item>> pagedListMutableLiveData;
+    MutableLiveData<PageKeyedDataSource<Integer, Item>> liveDataSource;
     QuestionRepository repository;
 
     public QuestionViewModel() {
